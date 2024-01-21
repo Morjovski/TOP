07ea40d76cb6a48b49191cb9a6a5f957d46ddb39
Morjovski <amor3ux@gmail.com>
2024-01-21 18:16:08 +0200

    complete validation style

--- JavaScript_Fullstack/sign-up-form/script.js
+++ JavaScript_Fullstack/sign-up-form/script.js
@@ -1,27 +1,27 @@
 const signUpLabels = document.querySelectorAll(".form-elem input");
-const invalidMsg = document.querySelectorAll('.form-elem .invalid-msg');
+const invalidMsg = document.querySelectorAll('.form-elem .validation-msg');
 const password = document.querySelector('#pwd');
 const passwordConfirm = document.querySelector('#pwd-confirm');
 
 signUpLabels.forEach((formInput, index) => {
-    formInput.addEventListener('input', () => {
-        if (!formInput.checkValidity()) {
-            formInput.classList.add('invalid');
-            invalidMsg[index].style.opacity = '1';
-        } else {
+    formInput.addEventListener('focusout', () => {
+        if (formInput.checkValidity()) {
             formInput.classList.remove('invalid');
             invalidMsg[index].style.opacity = '0';
-        }
+        } else {
+            formInput.classList.add('invalid');
+            invalidMsg[index].style.opacity = '1';
+        };
     });
 });
 
 passwordConfirm.addEventListener('input', () => {
-    const pwd = password.textContent;
-    const pwdConfirm = passwordConfirm.textContent;
-
-    if (pwdConfirm != pwd) {
-        pwdConfirm.classList.add('invalid');
+    const pwd = password.value;
+    const pwdConfirm = passwordConfirm.value;
+    
+    if (pwdConfirm == pwd) {
+        passwordConfirm.classList.remove('invalid');
     } else {
-        pwdConfirm.classList.remove('invalid');
+        passwordConfirm.classList.add('invalid');
     }
 });
